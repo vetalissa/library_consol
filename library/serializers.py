32d44c637c20cf7a732cae3314ba161ed1ece3fd
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-11-21 20:33:23 +0300

    crear abstract method for seriliazer

---
+++ library/serializers.py
@@ -0,0 +1,44 @@
+import json
+from libook import Book
+
+
+def create_json_library():
+    """
+    Создание пустого файла json.
+    """
+
+
+def load_json_library(library):
+    """
+    Загрузка книг из json.
+    """
+
+
+def add_json_library(book_instance: Book):
+    """
+    Добавление книг в json.
+    """
+
+
+def update_json_library(id_book: int, status: str):
+    """
+    Обновление статуса у книги в json.
+    """
+
+
+def delete_json_library(id_book: int):
+    """
+    Удаление книги из json.
+    """
+
+
+def unloading_json():
+    """
+    Выгружаем данные из файла json
+    """
+
+
+def loading_in_json(data):
+    """
+    Загружаем данные в файла json
+    """
