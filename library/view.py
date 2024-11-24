b4c6d07562f1bee91ebf85e90e7a00be49f28318
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-11-24 20:25:31 +0300

    create library append book

--- library/view.py
+++ library/view.py
@@ -4,6 +4,7 @@ from libook import Library
 library = Library()
 name_file = 'data.json'
 
+
 def get_file_name(action):
     """ Запрос у пользователя названия файла, и проверка его формата и наличия в директории. """
     while True:
@@ -63,3 +64,44 @@ def create_or_load_json():
             continue
 
         break
+
+
+def library_add_book():
+    """
+    Функция добавления книги(Book) в библиотеку(Library).
+    """
+    while True:
+        print('_' * 165)
+
+        book_val = {
+            'book_title': ['Введите название книги: ', ''],
+            'book_author': ['Введите автора: ', ''],
+            'book_year': ['Введите год издания: ', '']
+        }
+
+        try:
+            for key in book_val:
+                val = input(book_val[key][0])
+
+                if val == 'МЕНЮ':  # Выход в меню
+                    return
+
+                book_val[key][1] = val
+            else:
+                book = library.add_book(book_val['book_title'][1],
+                                        book_val['book_author'][1],
+                                        book_val['book_year'][1])
+
+                serializers.add_json_library(name_file, book)  # Добавление в файл data.json новой книги
+
+                print('_' * 165)
+                print(f'{book}\nКнига успешна добавлена в библиотеку.')
+                print('_' * 165)
+
+            if 'да' != input('Добавить еще одну книгу?(Да/Нет)\n').lower():
+                break
+
+        except ValueError:
+            print('\n' + '<!>' * 3 + 'ОШИБКА' + '<!>' * 50)
+            print('Вы ввели неверные данные...\nПопробуйте ещё раз.')
+            print('<!>' * 55 + '\n')
